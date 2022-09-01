b740a902354a17d6674b500ebd6a6064e99f122f
Pockethouse <101947734+Pockethouse@users.noreply.github.com>
2022-09-01 17:34:40 -0400

    Checklist application

---
+++ checklist/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  checklist
+//
+//  Created by Mark Bowen on 8/31/22.
+//
+//currently only serves to combine the text and the checked variables into one object.
+import Foundation
+
+class ChecklistItem: NSObject {
+  var text = ""
+  var checked = false
+}
